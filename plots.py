6e98110a79f36fbdfcdaa9ec24d73efdb01a7fdf
juanctkc <90647234+juanctkc@users.noreply.github.com>
2024-02-26 11:19:44 -0300

    Update plots.py

--- plots.py
+++ plots.py
@@ -3,21 +3,25 @@ from typing import List
 
 import matplotlib.pyplot as plt
 from datetime import datetime
-def create_time_plt(name: str, timings: List[List[int]], vertical_line_x: datetime = None, vertical_line_msg: str = None):
+def create_time_plt(name: str, timings: List[List[int]], vertical_line_date: str = None, vertical_line_msg: str = None):
     
     # print(timings);
     # print(type(vertical_line_x))
 
     plt.figure(figsize=(24, 12))
     x_values, y_values = zip(*timings)
-    if vertical_line_x is None or ((isinstance(vertical_line_x, str) and not vertical_line_x.strip())):
-        plt.axvline(x=datetime.strptime(vertical_line_x, "%Y-%m-%d"), color='r', linestyle='--', label=vertical_line_msg)
-        plt.text(vertical_line_x, max(y_values), vertical_line_msg, rotation=90, verticalalignment='bottom', ha='right')
+
+    # print (type(vertical_line_date))
+    # print (vertical_line_date)
+    if vertical_line_date:
+        vertical_line_date = datetime.strptime(vertical_line_date, "%Y-%m-%d")
+        plt.axvline(x=vertical_line_date, color='r', linestyle='--', label=vertical_line_msg)
+        plt.text(vertical_line_date, max(y_values), vertical_line_msg, rotation=90, verticalalignment='bottom', ha='right')
     plt.title(name)
     plt.xlabel("Workflow Run Dates")
     plt.ylabel("Duration in Minutes")
-    plt.scatter(x_values, y_values)
-    print(type(x_values[1]))
+    plt.scatter(x_values, y_values, label="Runs")
+    #print(type(x_values[1]))
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.legend()
